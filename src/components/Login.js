e6270c966a07a912ad9c4c3520366d721aa5ad72
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2023-08-23 16:28:12 +0300

    Done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Login.css'
+import fox from './assets/amma.png'
 
 function Login() {
     return (
         <div className='login'>
-            <img src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='logo' className='login__logo'/>
+            <img src={fox} alt='logo' className='login__logo'/>
+            <div className='login__signin'>
+                <h4>Sign in</h4>
+                <form>
+                    <label>E-mail</label>
+                    <input type='text' />
+                    <label>Password</label>
+                    <input type='text' />
+                    <button>Sign in</button>
+                </form>
+            </div>
         </div>
     )
 }
